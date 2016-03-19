bb756cd62a5a1bb13d9b00834dff3511395c99f9
Joshua N Boren <josh@epichousestudios.com>
2016-03-19 21:23:48 +0000

    Changes: -Compiling to AVX -Reverted Engine2D change to CRigidBody

--- oneEngine-basic/source/engine/physics/motion/CRigidbody.cpp
+++ oneEngine-basic/source/engine/physics/motion/CRigidbody.cpp
@@ -397,7 +397,7 @@ void CRigidBody::SetShape ( physShape* pNewShape )
 {
 	// Get the Read/Write copy of the rigidbody's collision info, and set the collision's new shape.
 	//pBody->getCollidableRw()->setShape( pNewShape );
-	pBody->setShape( pNewShape ); // Code is moved into physRigidbody
+	pBody->setShape( pNewShape->getShape() ); // Code is moved into physRigidbody
 }
 
 // Set new minimum penetration depth.
