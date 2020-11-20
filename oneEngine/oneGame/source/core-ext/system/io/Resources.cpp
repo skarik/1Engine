ce32536b7d42e19c98d98ce4d11e7b5d67b4fd7b
Josh B <josh@epichousestudios.com>
2020-11-19 20:13:32 -0800

    Updated default compiler to v141 (VS2017).

--- oneEngine/oneGame/source/core-ext/system/io/Resources.cpp
+++ oneEngine/oneGame/source/core-ext/system/io/Resources.cpp
@@ -5,7 +5,9 @@
 #ifdef _MSC_VER
 #	if _MSC_VER<=1900 // VS2015
 namespace fs = std::tr2::sys;
-#	else // VS2017+
+#	elif _MSC_VER<=19016 // VS2015
+namespace fs = std::experimental::filesystem;
+#	else // VS2019+
 namespace fs = std::filesystem;
 #	endif
 #else
